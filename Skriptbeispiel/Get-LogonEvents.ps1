7955c615a3263a7eff3fd052d54edd3a68d1c544
Stefan Ober <StefanO@ppedv.de>
2024-03-20 11:51:52 +0100

    Zusätzliche Ausgabe mit Verbose

--- Skriptbeispiel/Get-LogonEvents.ps1
+++ Skriptbeispiel/Get-LogonEvents.ps1
@@ -1,8 +1,14 @@
+[cmdletBinding()]
 param(
 [Parameter(Mandatory=$true)]
-$EventId,
-$Newest = 5 ,
-$Computername = "localhost"
+[int]$EventId,
+
+[int]$Newest = 5 ,
+
+[string]$Computername = "localhost"
 )
+
+Write-Verbose -Message "Zusätzliche Info"
+
 Get-EventLog -LogName Security -ComputerName $Computername | Where-Object EventId -eq $EventId | Select-Object -First $Newest
 
